257610dc10b0e5e8b46e52f5c9ce6b596a33081d
Tanatorn Kitmanamettamit <tanatornkab2019@gmail.com>
2019-08-17 11:51:12 +0700

    sucess get get:id put delete

--- app.js
+++ app.js
@@ -17,29 +17,27 @@ class Pokemon{
     }
 }
 
-let pokemons= []
-
-
-app.get('/pokemon', (req, res) => res.send(pokemons))
-
-app.post('/pokemon', (req, res) => {
-
-    if(req.body.name=== null||
-        req.body.name ===''||
-        req.body.type ===null||
-        req.body.type ===''
-
-        ){
-            res.sendStatus(400)
-
+let pokemons= [
+    
+    {
+        name: "Pokédex ",
+        type: "bug",
+        id: 1
+    },
+    {
+        name: "Sandslash",
+        type: "ground",
+        id: 2
+    },
+    {
+        name: "Raichu",
+        type: "electirc",
+        id: 3
     }
+]
+getObject('pikagu','tunder')
+getObject('Pokédex ','bug')
 
-    let tmp = getObject(req.body.name,req.body.type)
-    pokemons.push(tmp)
-    console.log(tmp)
-    //res.send('Still work in progreen...')
-    res.sendStatus(201)
-})
 function generateID (id){
     let newID = id+1
     return newID
@@ -49,7 +47,85 @@ function getObject(name,type){
     tmp.id  =  generateID (pokemons.length)
     return tmp
 }
+function checkError(v){
+   return  v!== null&&    v !==undefined&&    v !==''
+}
+function getpokemonbyID(id){
+    return pokemons[id -1 ] !==undefined && pokemons[id-1] !==null
+    let p = pokemons[id-1]
+    if(p===undefined){
+        res.status(400).send({error : "Cannot update pokemon:Pokemon is not found"})
+        return
+    }
+}
 
 
 
+app.get('/pokemon', (req, res) => res.send(pokemons))
+
+app.get('/pokemon/:id', (req, res) => {
+   let id = req.params.id
+   let p = pokemons[id-1]
+   res.send(p)
+})
+
+app.post('/pokemon', (req, res) => {
+    console.log(req.body)
+    if( !checkError(req.body.name)||
+        !checkError(req.body.type)
+            )
+
+        {
+            res.status(400).send({error : "Insuffucuent paramiters: name and type are required parameter"})
+            return
+    }
+
+    let tmp = getObject(req.body.name,req.body.type)
+    pokemons.push(tmp)
+    console.log(tmp)
+    //res.send('Still work in progreen...')
+    res.sendStatus(201)
+})
+
+app.put('/pokemon/:id', (req, res) =>{
+    
+    if(!checkError(req.body.type2)){
+        res.status(400).send({error : "Insuffucuent paramiters:type2 is required parameter"})
+        return
+    }
+    
+    if(!checkError(req.params.id)){
+        res.status(400).send({error : "Insuffucuent paramiters:id is required parameter"})
+        return
+    }
+    let id = req.params.id
+    let p = pokemons[id-1]
+    if(p===undefined){
+        res.status(400).send({error : "Cannot update pokemon:Pokemon is not found"})
+        return
+    }
+
+    p.type2=req.body.type2
+    pokemons[id-1]=p
+    res.sendStatus(201)
+})
+
+app.delete('/pokemon/:id',(req,res)=>{
+    
+    let id = req.params.id
+    if(!checkError(req.params.id)){
+        res.status(400).send({error : "Insuffucuent paramiters:type2 is required parameter"})
+        return
+    }
+    
+    let p = pokemons[id-1]
+    if(p===undefined){
+        res.status(400).send({error : "Cannot update pokemon:Pokemon is not found"})
+        return
+    }
+    delete pokemons[id -1]
+    res.sendStatus(204)
+
+
+})
 app.listen(port, () => console.log(`Pokemon API listening on port ${port}!`))
